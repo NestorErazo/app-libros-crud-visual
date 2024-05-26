e88c8af4b3cc7dd6d8358802d0174b45c8cd861f
NestorErazo <njerazo@gmail.com>
2024-05-26 01:03:14 -0500

    2 prueba

--- src/app.js
+++ src/app.js
@@ -1,55 +1,97 @@
 const express = require("express");
-const cors= require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
 const app = express();
-
-app.use(cors());
+const  mongoose = require("mongoose");
+const cors= require("cors");
+require("dotenv").config();
 app.use(express.json());
+app.use(cors());
 
-const mongoUri= process.env.MONGODB_URI;
+
+const mongoUri = process.env.MONGODB_URI;
+
 try {
     mongoose.connect(mongoUri);
-    console.log("conexion exitosa con base de datos",mongoUri);
-
+    console.log('connected to mongo');
 } catch (error) {
-    console.error("falla de conexion con base de datos",error);
+    console.error("error connecting to mongo",error);
 }
 
-const libroSchema = new mongoose.Schema({
-titulo: String,
-autor: String
-});
+    const libroSchema = new mongoose.Schema ({
+    titulo: String,
+    autor: String
+    });
 
-app.get('/', function (req, res) {
-res.send("ingresaste tienda libros");
-});
-
-//crear nuevo libro
-const Libro = mongoose.model("Libro",libroSchema);
+const Libro = mongoose.model("Libro", libroSchema);
+//crear libro schema
 app.post("/libro", async (req, res) => {
-
     const libro = new Libro({
         titulo: req.body.titulo,
-        autor: req.body.autor
+         autor: req.body.autor
     })
     try {
         await libro.save();
         res.json(libro);
     } catch (error) {
-        console.error("error al guardar el libro".error);
+        console.error("error save book");
     }
 })
+//ver libros creados
+    app.get("/libro",async (req, res)=>{
+        try {
+            const libros = await Libro.find();
+            res.json(libros);
+        } catch (error) {
+            console.status(500).send("error get books");
+        }
+    });
+    //buscar libro
+    app.get("/libro/:id", async (req, res) => {
+        try {
+          const libro = await Libro.findById(req.params.id);
+          if (libro) {
+            res.json(libro);
+          } else {
+            res.status(404).send("Libro no encontrado");
+          }
+        } catch (error) {
+          res.status(500).send("Error al buscar el libro");
+        }
+      });
 
-//traer listado de libros
-app.get("/libros", async (req, res)=>{
+//actualizar libro
+app.put("/libro/:id", async (req, res) => {
     try {
-        const libros = await Libro.find();
-        res.json(libros);
+      const libro = await Libro.findByIdAndUpdate(
+        req.params.id,
+        {
+          titulo: req.body.titulo,
+          autor: req.body.autor,
+        },
+        { new: true } 
+      );
+
+      if (libro) {
+        res.json(libro);
+      } else {
+        res.status(404).send("Libro no encontrado");
+      }
     } catch (error) {
-        res.status().send("error al traer los libros ",error);
+      res.status(500).send("Error al actualizar el libro");
     }
-})
+  });
+  //eliminar libro
+  app.delete("/libro/:id", async (req, res) => {
+    try {
+        const libro = await Libro.findOneAndDelete({ _id: req.params.id });
+        if (libro) {
+            res.status(204).send();
+        } else {
+            res.status(404).send("Libro no encontrado");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error al eliminar el libro");
+    }
+});
 
 module.exports =app;
-
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const app= require("./app");
+const app=require('./app');
 
-const PORT= process.env.PORT ||3000;
- app.listen(PORT,() => {
-    console.log("servidor corriendo en el puerto http://localhost:3000/");
-    }
- )
+const PORT =process.env.PORT ||3000;
+app.listen(PORT, ()=>{
+console.log('listening on port https://localhost:3000');
+});
+
 
